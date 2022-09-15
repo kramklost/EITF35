9f1fb6994902027b3f2ef146fd898973859a6bfb
kramklost <89154375+kramklost@users.noreply.github.com>
2022-09-15 17:08:06 +0200

    Add files via upload

--- project_2/project_2.sim/sim_1/behav/elaborate.bat
+++ project_2/project_2.sim/sim_1/behav/elaborate.bat
@@ -1,6 +1,6 @@
 @echo off
 set xv_path=C:\\Xilinx\\Vivado\\2016.1\\bin
-call %xv_path%/xelab  -wto 7d42f00fe84a41448182d7f1b1b83400 -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L secureip --snapshot tb_keyboard_behav xil_defaultlib.tb_keyboard -log elaborate.log
+call %xv_path%/xelab  -wto c6b32340fad14973a908724499597a4a -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L secureip --snapshot tb_keyboard_behav xil_defaultlib.tb_keyboard -log elaborate.log
 if "%errorlevel%"=="0" goto SUCCESS
 if "%errorlevel%"=="1" goto END
 :END
